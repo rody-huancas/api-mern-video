11da8a8dc65fd987488315494bf2c8ae9dd2657a
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-01 12:25:09 -0500

    Add: crud complete

--- src/routes/videos.controller.ts
+++ src/routes/videos.controller.ts
@@ -1,21 +1,39 @@
 import { RequestHandler } from 'express';
+import Video from './Video'
 
-export const createVideo: RequestHandler = (req, res) => {
-    res.json("create video")
+export const createVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findOne({ url: req.body.url })
+    if (videoFound)
+        return res.status(301).json({ message: "La URL ya existe" })
+
+    const video = new Video(req.body);
+    const savedVideo = await video.save();
+    res.json(savedVideo)
 }
 
-export const getVideos: RequestHandler = (req, res) => {
-    res.json("Getting video")
+export const getVideos: RequestHandler = async (req, res) => {
+    try {
+        const videos = await Video.find()
+        return res.json(videos)
+    } catch (error) {
+        res.json(error)
+    }
 }
 
-export const getVideo: RequestHandler = (req, res) => {
-    res.json("Getting video")
+export const getVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findById(req.params.id);
+    if (!videoFound) return res.status(204).json();
+    return res.json(videoFound);
 }
 
-export const deleteVideo: RequestHandler = (req, res) => {
-    res.json("Getting video")
+export const deleteVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findByIdAndDelete(req.params.id);
+    if (!videoFound) return res.status(204).json();
+    return res.json(videoFound);
 }
 
-export const updateVideo: RequestHandler = (req, res) => {
-    res.json("Getting video")
+export const updateVideo: RequestHandler = async (req, res) => {
+    const videoUpdated = await Video.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!videoUpdated) return res.status(204).json();
+    return res.json(videoUpdated);
 }
